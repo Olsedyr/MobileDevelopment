c981e79b9672479eca3ddcf74d6d526d3f0fa812
Jonas Larsen <joonaslarsen@gmail.com>
2024-09-26 08:33:33 +0200

    fix: Remove react-native-dotenv and use expo environment variables

--- nativeWheels/axios/instance.ts
+++ nativeWheels/axios/instance.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
-import { API_BASE_URL } from "@env";
 
 const instance = axios.create({
-  baseURL: `${API_BASE_URL}/api`,
+  baseURL: `${process.env.EXPO_PUBLIC_API_URL}/api`,
 });
 
 export default instance;
